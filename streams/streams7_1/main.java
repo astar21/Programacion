13e6c4b9c40eef47e5b57d624ad1377dd4cb10f4
astar21 <marco_astar@hotmail.es>
2018-05-08 00:55:49 +0200

    streams  java
    streams  java

---
+++ streams/streams7_1/main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streams7_1;
+import java.io.IOException;
+/**
+ *
+ * @author Marco
+ */
+public class main {
+     public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        
+       Streams7.muestra("C:\\Users\\Marco\\Desktop\\ejercicio1.txt");
+    
+        
+        
+    }
+}
